adbcf773f6a1c7754871372dfcb8d7b55b5e9024
MokaSrinu <srinumoka75@gmail.com>
2024-11-19 23:46:33 +0530

    feat(python): added exmaples for variables and constants

---
+++ backend/python/examples/variables_and_constants1.py
@@ -0,0 +1,11 @@
+# Variables
+count = 100
+emp_name = 'Srinu'
+age1, age2, age3 = 44, 37, 22
+print(count, emp_name, age1, age2, age3)
+
+
+# Constants -- Capital Letter Naming
+PI = 3.14
+GRAVITY = 9.8
+print(PI, GRAVITY)
