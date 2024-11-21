23924978a2930fafdb94fa641ade17443694b82d
MokaSrinu <srinumoka75@gmail.com>
2024-11-21 22:41:13 +0530

    feat(python): added examples for python different modules

---
+++ backend/python/examples/python_different_modules8.py
@@ -0,0 +1,11 @@
+# Importing Module as well as Renaming it.
+import math as m
+print('The Value of PI is:', m.pi)
+
+# usage of "from"
+from math import pi
+print('The value of PI is:', pi)
+
+# import all names from the standard module math
+from math import *
+print('The value of PI is:', pi)
