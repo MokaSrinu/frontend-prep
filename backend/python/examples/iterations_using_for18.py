0dead25d6ab94562575582710e0f31863b513162
MokaSrinu <srinumoka75@gmail.com>
2024-11-24 18:05:00 +0530

    feat(python): added examples for python iterator using for

---
+++ backend/python/examples/iterations_using_for18.py
@@ -0,0 +1,15 @@
+# print all the elements present in a set
+a = [1, 2, 3, 4, 5]
+for i in a:
+    print(i)
+
+# using range in for loop
+for i in range(6, 11):
+    print(i)
+
+# using for loop for printing values in the tuple and using the else command in for loop
+b = (11, 12, 13, 14, 15)
+for i in b:
+    print(i)
+else:
+    print('Printing Completed!')
