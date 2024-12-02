e75dc1ce385b376e2b52f3a4b06678e41bfbfaa7
MokaSrinu <srinumoka75@gmail.com>
2024-12-02 23:28:05 +0530

    feat(python): added examples for python matrix implementation

---
+++ backend/python/examples/matrix_implementation37.py
@@ -0,0 +1,14 @@
+# a is a 2D matrix with integers
+
+a = [['Roy', 80, 75, 85, 90, 95],
+    ['John', 75, 80, 75, 85, 100],
+    ['Dave', 80, 80, 80, 90, 95]]
+
+# b is a nested list but not a matrix
+b = [['Roy', 80, 75, 85, 90, 95],
+    ['John', 75, 80, 75], # because it has 4 elements
+    ['Dave', 80, 80, 80, 90, 95]]
+
+print(a)
+print()
+print(b)
