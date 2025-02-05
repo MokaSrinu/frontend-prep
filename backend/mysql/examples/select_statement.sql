a68695001e06483078d23183f53d2d336a8a6e6e
MokaSrinu <srinumoka75@gmail.com>
2025-02-05 22:43:21 +0530

    feat(mysql): examples for select statement

---
+++ backend/mysql/examples/select_statement.sql
@@ -0,0 +1,10 @@
+SELECT * 
+FROM Parks_and_Recreation.employee_demographics;
+
+SELECT first_name, last_name, birth_date, age, (age+10)*10 inc_age 
+FROM Parks_and_Recreation.employee_demographics;
+
+# PEMDAS
+
+SELECT DISTINCT gender
+FROM Parks_and_Recreation.employee_demographics;
